d03be19716c865960e9ed5d30ead4bb3030b2705
boaborn <michaelzhaojq@hotmail.com>
2018-05-17 23:11:43 +1200

    add mobx

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react'
+import { observable } from 'mobx'
+import { observer } from "mobx-react"
+@observer
 export default class App extends Component {
+    @observable temp = 'hello '
+
     render(){
         return(
             <div>
-                <h1> My React App !!</h1>
+                <h1> My React App !!!{this.temp}</h1>
             </div>
         )
     }
